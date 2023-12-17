ff14ec85a9e88d1041aaa4eab6c4d6f5b94ec692
zenpk <99558412+zenpk@users.noreply.github.com>
2023-12-17 17:41:44 +0800

    remove touch

--- runner/mkdirs.sh
+++ runner/mkdirs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# mkdir for world, backup and server. Then create log file
+# mkdir for world, backup and server
 set -e
 echo "creating folders"
 mkdir -p "$1"
 mkdir -p "$2"
 mkdir -p "$3"
-echo "creating log file"
-touch "$4"
 echo "done"
